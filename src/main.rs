9a6e9a80b208f494d7a3f74c7c4f1e38a055cb9c
Rofdo <git.hub@ewe.net>
2024-02-04 13:15:23 +0100

    implemented basic picking

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use rand::Rng;
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -7,7 +8,21 @@ struct Args {
     players: usize,
     /// The number of civilizations for each player to choose from
     #[arg(short, long)]
-    civs: usize,
+    picks: usize,
+}
+
+type Civ = u32;
+
+fn choose_civs(civs: &mut Vec<Civ>, players: usize, picks: usize) -> Vec<Vec<u32>> {
+    let mut result = vec![vec![]; players];
+    for _ in 0..picks {
+        for player in 0..players {
+            let index = rand::thread_rng().gen_range(0..civs.len());
+            let civ = civs.remove(index);
+            result[player].push(civ);
+        }
+    }
+    result
 }
 
 fn main() {
