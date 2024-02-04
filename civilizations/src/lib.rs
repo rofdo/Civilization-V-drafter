e7be6206e13b4861c4b813b0f3b7b65567f309e0
Rofdo <git.hub@ewe.net>
2024-02-04 15:45:42 +0100

    added proc macro for civs (won't use)

--- civilizations/src/lib.rs
+++ civilizations/src/lib.rs
@@ -1,10 +1,59 @@
-use proc_macro;
+use serde::{Deserialize, Serialize};
+use quote::quote;
+
+#[derive(Debug, Clone, Serialize, Deserialize, Copy)]
+enum Bias {
+    Coastal,
+    // also may be called Hills
+    #[serde(alias = "Hills")]
+    Hill,
+    Jungle,
+    Desert,
+    Grassland,
+    Tundra,
+    Forest,
+    AvoidJungle,
+    AvoidDesert,
+    AvoidTundra,
+    None,
+}
+
+trait Civ {
+    fn name(&self) -> &str;
+    fn leader(&self) -> &str;
+    fn bias(&self) -> Vec<Bias>;
+}
+
+use proc_macro::TokenStream;
+#[proc_macro_derive(Civ)]
+pub fn derive_leader(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+
+    impl_civ(&ast)
+}
+
+fn impl_civ(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl Civ for #name {
+            fn name(&self) -> &str {
+                &self.name
+            }
+            fn leader(&self) -> &str {
+                &self.leader
+            }
+            fn bias(&self) -> Vec<Bias> {
+                self.bias
+            }
+        }
+    };
+    gen.into()
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-    }
+    fn it_works() {}
 }
