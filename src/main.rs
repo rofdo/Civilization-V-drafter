1bb676434304fec88fc3369215836a41df91bdc7
Rofdo <git.hub@ewe.net>
2024-02-04 12:53:19 +0100

    added args struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Args {
+    /// The number of players to play the game
+    #[arg(short, long)]
+    players: usize,
+    /// The number of civilizations for each player to choose from
+    #[arg(short, long)]
+    civs: usize,
+}
+
 fn main() {
     println!("Hello, world!");
 }
